2164b69d836662a5d233f839a398365b428a5736
高橋大典 <takahashidaisuke@makku.local>
2024-11-03 18:40:53 +0900

    add: Create newapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header() {
+  return <h1>Hello world</h1>;
+}
+
+function Heading() {
+  return <h1>This is an h1 heading</h1>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Heading />;
+  // return <Header />;
 }
 
 export default App;
